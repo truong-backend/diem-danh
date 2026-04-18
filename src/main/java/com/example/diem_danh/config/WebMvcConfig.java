f39d626d0b3acfed796ec245d642d4a65d401406
Truong back-end <honguyententhanhtruong@gmail.com>
2026-04-19 04:37:35 +0700

    Update deploy

--- src/main/java/com/example/diem_danh/config/WebMvcConfig.java
+++ src/main/java/com/example/diem_danh/config/WebMvcConfig.java
@@ -13,7 +13,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .allowedOrigins(
                         "http://localhost:5173",
                         "http://localhost:3000",
-                        "https://diem-danh-dev-truong.vercel.app"
+                        "http://91.98.227.3:8082"
                 )
                 .allowedMethods("*")
                 .allowedHeaders("*")
