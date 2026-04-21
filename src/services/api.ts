f3a4d70afb4e82fc720463c8570fc56aec3654d2
Truong back-end <honguyententhanhtruong@gmail.com>
2026-04-21 19:29:26 +0700

    remove /api

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios, { AxiosError } from 'axios'
 import { useAuthStore } from '../store/authStore'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL + '/api',
+  baseURL: import.meta.env.VITE_API_URL',
   headers: { 'Content-Type': 'application/json' },
   timeout: 15000,
 })
