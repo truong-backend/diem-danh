1dd2835677b78310d221953a30d368c7358b4fbc
Truong back-end <honguyententhanhtruong@gmail.com>
2026-05-13 19:05:45 +0700

    chore: update Vite configuration and add README file

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   define: {
-    global: 'window', // thêm dòng này để fix sockjs-client lỗi global
+    global: 'window', // fix sockjs-client lỗi global
   },
   server: {
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://localhost:8082',
         changeOrigin: true,
       }
     }
